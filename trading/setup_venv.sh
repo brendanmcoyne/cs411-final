478b2d36d521ccfe06ac87db3a1b3f0e136f5aca
llotze <llotze@bu.edu>
2025-04-27 18:00:10 -0400

    created gitignore

--- trading/setup_venv.sh
+++ trading/setup_venv.sh
@@ -7,17 +7,19 @@ REQUIREMENTS_FILE="requirements.txt"
 # Check if the virtual environment already exists
 if [ ! -d "$VENV_DIR" ]; then
   echo "Creating virtual environment..."
-
-
-  # Install dependencies from requirements.lock if it exists
-  if [ -f "$REQUIREMENTS_FILE" ]; then
-    echo "Installing dependencies from $REQUIREMENTS_FILE..."
-
-  else
-    echo "Error: $REQUIREMENTS_FILE not found."
-    exit 1
-  fi
-else
-
-  echo "Virtual environment already exists. Activated."
+  python3 -m venv "$VENV_DIR"
 fi
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Install dependencies
+if [ -f "$REQUIREMENTS_FILE" ]; then
+  echo "Installing dependencies from $REQUIREMENTS_FILE..."
+  pip install --no-cache-dir -r "$REQUIREMENTS_FILE"
+else
+  echo "Error: $REQUIREMENTS_FILE not found."
+  exit 1
+fi
+
+echo "Virtual environment setup complete."
